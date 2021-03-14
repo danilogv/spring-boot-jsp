3583cb16d0c7007fe3bfae62725af9e3ce22ecc1
Danilo <danilo.gv@hotmail.com>
2021-03-13 23:57:20 -0300

    Criando as funções no repositório

---
+++ src/main/java/br/com/springbootjsp/modelo/repositorio/EmpresaRepositorio.java
@@ -0,0 +1,55 @@
+package br.com.springbootjsp.modelo.repositorio;
+
+import br.com.springbootjsp.modelo.dominio.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EmpresaRepositorio extends JpaRepository<Empresa,UUID> {
+
+    @Modifying
+    @Query(value =
+        "INSERT INTO empresa(nome,cnpj) " +
+        "VALUES (?1,?2)"
+    ,nativeQuery = true)
+    void insere(String nome,String cnpj,String funcionarioId);
+
+    @Modifying
+    @Query(value =
+        "UPDATE empresa " +
+        "SET nome = ?1,cnpj = ?2 " +
+        "WHERE id = ?3"
+    ,nativeQuery = true)
+    void altera(String nome,String cnpj,String id);
+
+    @Modifying
+    @Query(value =
+        "DELETE FROM empresa " +
+        "WHERE id = ?1"
+    ,nativeQuery = true)
+    void remove(String id);
+
+    @Query(value =
+        "SELECT * " +
+        "FROM empresa"
+    ,nativeQuery = true)
+    List<Empresa> buscarTodos();
+
+    @Query(value =
+        "SELECT * " +
+        "FROM empresa " +
+        "WHERE id = ?1"
+    ,nativeQuery = true)
+    Empresa buscar(String id);
+
+    @Query(value =
+        "SELECT COUNT(id) > 0 " +
+        "FROM empresa " +
+        "WHERE cnpj = ?1"
+    ,nativeQuery = true)
+    Boolean existe(String cnpj);
+
+}
---
+++ src/main/java/br/com/springbootjsp/modelo/repositorio/FuncionarioRepositorio.java
@@ -0,0 +1,58 @@
+package br.com.springbootjsp.modelo.repositorio;
+
+import br.com.springbootjsp.modelo.dominio.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public interface FuncionarioRepositorio extends JpaRepository<Funcionario,UUID> {
+
+    @Modifying
+    @Query(value =
+        "INSERT INTO funcionario(nome,cpf,salario,idade,data_admissao,empresa_id) " +
+        "VALUES (?1,?2,?3,?4,?5,?6)"
+    ,nativeQuery = true)
+    void insere(String nome, String cpf, BigDecimal salario,Date dataAdmissao,String empresaId);
+
+    @Modifying
+    @Query(value =
+        "UPDATE funcionario " +
+        "SET nome = ?1,cpf = ?2,salario = ?3,idade = ?4,data_admissao = ?5,empresa_id = ?6 " +
+        "WHERE id = ?7"
+    ,nativeQuery = true)
+    void altera(String nome, String cpf, BigDecimal salario,Date dataAdmissao,String empresaId,String id);
+
+    @Modifying
+    @Query(value =
+        "DELETE FROM funcionario " +
+        "WHERE id = ?1"
+    ,nativeQuery = true)
+    void remove(String id);
+
+    @Query(value =
+        "SELECT * " +
+        "FROM funcionario"
+    ,nativeQuery = true)
+    List<Funcionario> buscarTodos();
+
+    @Query(value =
+        "SELECT * " +
+        "FROM funcionario " +
+        "WHERE id = ?1"
+    ,nativeQuery = true)
+    Funcionario buscar(String id);
+
+    @Query(value =
+        "SELECT COUNT(id) > 0 " +
+        "FROM funcionario " +
+        "WHERE cpf = ?1 " +
+        "OR (cpf = ?1 AND empresa_id = ?2)"
+    ,nativeQuery = true)
+    Boolean existe(String cpf,String empresaId);
+
+}
