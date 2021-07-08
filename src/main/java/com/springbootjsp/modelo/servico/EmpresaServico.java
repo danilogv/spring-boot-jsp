29de68445cb4bcd85d4d2f68645558fb78e8c5c7
Danilo <danilo.gv@hotmail.com>
2021-07-08 12:29:21 -0300

    Colocando estilo na tela de visualizar empresa

--- src/main/java/com/springbootjsp/modelo/servico/EmpresaServico.java
+++ src/main/java/com/springbootjsp/modelo/servico/EmpresaServico.java
@@ -18,36 +18,36 @@ public class EmpresaServico {
     @Autowired
     private EmpresaRepositorio repositorio;
 
-    @Transactional(isolation = Isolation.READ_COMMITTED, readOnly = true)
+    @Transactional(isolation = Isolation.READ_COMMITTED,readOnly = true)
     public List<Empresa> listar(String nome) {
         List<Empresa> empresas = this.repositorio.buscarTodos(nome);
         return empresas;
     }
 
-    @Transactional(isolation = Isolation.READ_COMMITTED, readOnly = true)
+    @Transactional(isolation = Isolation.READ_COMMITTED,readOnly = true)
     public Empresa buscar(String id) {
         Empresa empresa = this.repositorio.buscar(id);
         return empresa;
     }
 
-    @Transactional(isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
+    @Transactional(isolation = Isolation.READ_COMMITTED,rollbackFor = Exception.class)
     public void inserir(Empresa empresa) {
         String id = UUID.randomUUID().toString();
         String nome = empresa.getNome();
         String cnpj = empresa.getCnpj();
-        if (!this.repositorio.existe(cnpj).equals(0)) {
+        if (this.repositorio.existe(cnpj).equals(1)) {
             String msg = "Empresa com CNPJ já cadastrado.";
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,msg);
         }
         this.repositorio.insere(id,nome,cnpj);
     }
 
-    @Transactional(isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
+    @Transactional(isolation = Isolation.READ_COMMITTED,rollbackFor = Exception.class)
     public void alterar(Empresa empresa) {
         String id = empresa.getId();
         String nome = empresa.getNome();
         String cnpj = empresa.getCnpj();
-        if (this.buscar(id).getCnpj().compareTo(cnpj) != 0 && !this.repositorio.existe(cnpj).equals(0)) {
+        if (!this.buscar(id).getCnpj().equals(cnpj) && this.repositorio.existe(cnpj).equals(1)) {
             String msg = "Empresa com CNPJ já cadastrado.";
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,msg);
         }
--- src/main/webapp/jsp/visualizar_empresa.jsp
+++ src/main/webapp/jsp/visualizar_empresa.jsp
@@ -1,18 +1,21 @@
 <%@ page contentType="text/html;charset=utf-8" pageEncoding="utf-8" %>
-<%@ taglib prefix="c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="utf-8" />
         <title> Empresa </title>
+        <link rel="stylesheet" type="text/css" href="http://localhost:8080/css/bootstrap.css" />
     </head>
     <body>
-        <h1> ${empresa.nome} </h1>
-        <p>
-            <label> CNPJ : ${empresa.cnpj} </label>
-        </p>
-        <a href="/empresas">
-            <button> Voltar </button>
-        </a>
+        <div class="mt-2 ml-2">
+            <h1> ${empresa.nome} </h1>
+            <br/>
+            <p class="font-weight-light"> CNPJ : ${empresa.cnpj} </p>
+            <br/>
+            <a href="/empresas">
+                <button type="button" class="btn btn-primary"> Voltar </button>
+            </a>
+        </div>
     </body>
 </html>
